0476140b5af504ae068eaf5050f290ccfee69bfb
Quadrant <Quadrant@DESKTOP-5K9DCI5>
2021-09-04 19:37:42 +0530

    Add project files.

---
+++ dictionary/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+            //key(unique)-value(any) pairs
+
+
+            Dictionary<string, string> names = new Dictionary<string, string>();
+
+
+            names.Add("1", "Asma");
+
+
+            names.Add("2", "Ashu");
+
+
+            names.Add("3", "mohammed");
+
+
+            names.Add("4", "Asma");
+
+
+
+
+
+
+
+
+            foreach (KeyValuePair<string, string> kv in names)
+
+
+            {
+
+
+                Console.WriteLine(kv.Key + " " + kv.Value);
+
+
+            }
+
+        }
+    }
+}
+    
---
+++ queue/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+namespace queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            //FIFO(FIRST IN FIRST OUT)
+            Queue<string> names = new Queue<string>();
+            names.Enqueue("Asma");
+            names.Enqueue("Ashu");
+            names.Enqueue("Sana");
+            names.Enqueue("Ashu");
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+            Console.WriteLine("Peek element: " + names.Peek());
+            Console.WriteLine("Dequeue: " + names.Dequeue());
+            Console.WriteLine("After Dequeue, Peek element: " + names.Peek());
+        }
+    }
+}
